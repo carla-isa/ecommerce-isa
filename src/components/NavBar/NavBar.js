b995c2d3759455d90441366cb7f7ea75ee3bab95
Carla Isa <isa.carlamarina@gmail.com>
2021-05-31 19:39:07 -0300

    Creación NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <>
+        <h1>Mica Saraceno Tattoo</h1>
+        <p><a href="#">About</a></p>
+        <p><a href="#">Turnos</a></p>
+        <p><a href="#">Tienda</a></p>
+        </>
+    )
+};
+
+export default NavBar;
